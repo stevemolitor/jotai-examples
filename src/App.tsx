3923139fc43c3af9307eec2eea6129ba8594a7e5
Steve Molitor <steve.molitor@zapier.com>
2023-09-19 12:39:49 -0500

    tweak some styles and such

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import { Suspense } from "react";
-import { atom, useAtom } from "jotai";
-import "./styles.css";
-
-const countAtom = atom(0);
-
-const Counter = () => {
-  const [count, setCount] = useAtom(countAtom);
-  const inc = () => setCount((c) => c + 1);
-  return (
-    <>
-      {count} <button onClick={inc}>+1</button>
-    </>
-  );
-};
+import { ReadWriteAtomExample } from "./read-write-atom/ReadWriteAtomExample";
 
 const App = () => (
   <Suspense fallback="Loading...">
     <div className="App">
       <h1>Jotai Examples</h1>
-      <Counter />
+
+      <ReadWriteAtomExample />
     </div>
   </Suspense>
 );
