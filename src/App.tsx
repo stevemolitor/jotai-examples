3a5655252bf1584b34f27aa26cde3d4b2890daae
Steve Molitor <stevemolitor@gmail.com>
2023-09-19 10:41:35 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Suspense } from "react";
+import { atom, useAtom } from "jotai";
+import "./styles.css";
+
+const countAtom = atom(0);
+
+const Counter = () => {
+  const [count, setCount] = useAtom(countAtom);
+  const inc = () => setCount((c) => c + 1);
+  return (
+    <>
+      {count} <button onClick={inc}>+1</button>
+    </>
+  );
+};
+
+const App = () => (
+  <Suspense fallback="Loading...">
+    <div className="App">
+      <h1>Hello Jotai</h1>
+      <h2>Enjoy coding!</h2>
+      <Counter />
+    </div>
+  </Suspense>
+);
+
+export default App;
