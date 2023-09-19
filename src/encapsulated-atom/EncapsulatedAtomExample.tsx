81505481ea398523d11fa5b772dec7d18cdc71e5
Steve Molitor <steve.molitor@zapier.com>
2023-09-19 16:50:06 -0500

    Add reset to EncapsulatedAtomExample

--- src/encapsulated-atom/EncapsulatedAtomExample.tsx
+++ src/encapsulated-atom/EncapsulatedAtomExample.tsx
@@ -1,15 +1,16 @@
 import { useCount } from "./state";
 
 export const EncapsulatedAtomExample = () => {
-  const { count, increment, decrement } = useCount();
+  const { count, increment, decrement, reset } = useCount();
 
   return (
     <div className="example">
       <>
-        <div className="example-title">Read / Write Atom Example:</div>
+        <div className="example-title">Encapsulated Atom Example:</div>
         <div>
           count: {count} <button onClick={increment}>increment</button>{" "}
-          <button onClick={decrement}>decrement</button>
+          <button onClick={decrement}>decrement</button>{" "}
+          <button onClick={reset}>reset</button>
         </div>
       </>
     </div>
